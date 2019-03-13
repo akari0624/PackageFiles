ceb686352c815ca593cd1415d6e63378f835ab4e
akari0624 <akari0624@hotmail.com>
2019-03-13 12:03:16 +0800

    seeking a good way to do copyfile in async and can return all the processing message

--- electron_side/src/ipcHandlers/index.ts
+++ electron_side/src/ipcHandlers/index.ts
@@ -1,16 +1,31 @@
 import { IPCKeys } from '../ipcChannel/ipcChannelKey';
-import OnPackFileDataFromClient from '../models/data_types/onPackFileDataFromClient'
+import OnPackFileDataFromClient from '../models/data_types/onPackFileDataFromClient';
+import { copyFile } from 'fs';
+import { promisify } from 'util';
+
+const copyFilePromisifyly = promisify(copyFile);
 
 const IPCHandlers = {
-  doFilesPack: (evt: Event, theObj: OnPackFileDataFromClient) => {
-    console.log(`:${IPCKeys.packTheseFiles}`);
+  doFilesPack: async (theObj: OnPackFileDataFromClient): Promise<string[]> => {
+    const { selectedFilesPath, sourceDirRootPath, distDirRootPath } = theObj;
+    const filesArr = selectedFilesPath.split('\r\n');
 
-    console.log(theObj.selectedFilesPath);
-
-    console.log(theObj.sourceDirRootPath);
-
-    console.log(theObj.distDirRootPath);
-    console.log('let do nodejs fs work~');
+    let processMsg: string[] = [];
+    await filesArr.forEach(async f => {
+      await copyFilePromisifyly(
+          `${sourceDirRootPath}${f}`,
+          `${distDirRootPath}${f}`,
+        ).then(() => {
+          processMsg.push(
+              `copy file from ${sourceDirRootPath}${f} to ${distDirRootPath}${f}`
+            );
+        }).catch(err => {
+          processMsg.push(
+            `handle copy file ${sourceDirRootPath}${f} occured error : ${err}`
+          );
+        });
+    });
+    return processMsg;
   }
 };
 
